4e157e49257d2ec010858d0804769167a20449a9
ZenomSoldark <52134544+ZenomSoldark@users.noreply.github.com>
2020-11-15 10:05:13 +0900

    Prismのフォルダ構成に変更
    Viewsフォルダ、ViewModelsフォルダを作成

--- WpfTemplate/Views/MainWindow.xaml.cs
+++ WpfTemplate/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfTemplate
+namespace WpfTemplate.Views
 {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
